41b82b40c503b3592fe0f3594db23c9ade9bdab1
«IrenSk» <skiv.study@gmail.com>
2023-03-21 01:37:35 +0300

    Add solution hw_6 task_2

--- Hw_6/hw_6_2.py
+++ Hw_6/hw_6_2.py
@@ -1,3 +1,24 @@
 # Задача 2 - Определить индексы элементов массива (списка),
 # значения которых принадлежат заданному диапазону
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+
+d1 = int(input("Укажите нижний порог диапазона: "))
+d2 = int(input("Укажите верхний порог диапазона: "))
+n = int(input("Количество чисел в массиве: "))
+
+list_rand = [random.randint(-10, 20) for _ in range(n)]
+print(list_rand)
+
+print("Индексы элементов, которые соответствуют диапазону: ")
+ind = [i for i, v in enumerate(list_rand) if d1 <= v <= d2]
+print(ind)
+
+# print([i for i, v in enumerate(list_rand) if d1 <= v <= d2])
+
+
+# развернутая версия
+# for i, v in enumerate(list_rand):
+#     if d1 <= v <= d2:
+#         print(i, end=" ")
